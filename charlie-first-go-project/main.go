2138d5de72ae04dd6499c5f22ca6435a874ae0a9
amaise <amaise@live.com>
2021-01-14 21:17:23 +0000

    Added main.go - missing from last commit.

---
+++ charlie-first-go-project/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var value int = 420
+	fmt.Println(value)
+	var f float32 = 3.14
+	fmt.Println(f)
+	// float you have to define if you need 32 or 64 bit.
+	firstName := "Charlie"
+	fmt.Println(firstName)
+}
